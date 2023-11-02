1e225893c7657e78477957c95fb3638ce9269713
GalisGalisteo <galis.travelling@gmail.com>
2023-11-02 15:03:56 +0100

    finishing ex with bonus 2, needs refactor

--- app.js
+++ app.js
@@ -1,35 +1,82 @@
-const fetchPokemon = async (pokemon) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+const fetchPokemons = async () => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`);
     if (!response.ok) {
-        throw new Error('Error fetching data');
+        throw new Error('Error fetching all pokemons');
     }
     const data = await response.json();
     return data;
 }
 
+const fetchPokemon = async (url) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Error fetching teh pokemon');
+    }
+    const data = await response.json();
+    return data;
+}
 
-let pokemon;
-const pokemonImg = document.querySelector('#pokemon-img');
+let pokemons
 
-document.querySelector('#select-pokemon').addEventListener('change', async (event) => {
-    const pokemonName = document.querySelector('#pokemon-name');
+const selectPokemon = document.querySelector('#select-pokemon');
+
+const addOptions = () => {
+    pokemons.results.forEach(pokemon => {
+        const option = document.createElement('option');
+        const name = pokemon.name;
+        const capitalizeName = name.charAt(0).toUpperCase() + name.slice(1);
+        option.value = name;
+        option.innerHTML = capitalizeName;
+        selectPokemon.appendChild(option);
+    })
+}
+
+const pokemonName = document.querySelector('#pokemon-name');
+
+window.addEventListener('load', async () => {
     try {
-        pokemon = await fetchPokemon(event.target.value);
-        if (pokemon) {
-            shinyBtn.classList.remove('disabled')
-        }
+        pokemons = await fetchPokemons();
+        addOptions();
     } catch (error) {
-        pokemonName.textContent = "Ups! I can't find the pokemon! Please, try again later!"
+        pokemonName.textContent = "Ups! I can't find the pokemons! Please, try again later!"
         console.error(error);
     }
-    pokemonName.textContent = pokemon.name;
-    pokemonImg.src = pokemon.sprites.front_default;
+})
+
+
+const pokemonImg = document.querySelector('#pokemon-img');
+
+let pokemonFinded;
+selectPokemon.addEventListener('change', async (event) => {
+    if (pokemons) {
+        shinyBtn.classList.remove('disabled')
+    }
+
+
+    const pokemonValue = event.target.value;
+    pokemonName.textContent = pokemonValue;
+
+    const pokemonToFind = pokemons.results.find(pokemon => pokemonValue === pokemon.name);
+
+    try {
+        pokemonFinded = await fetchPokemon(pokemonToFind.url)
+    } catch (error) {
+        pokemonName.textContent = "Ups! I can't find the pokemon you selected! Please, try again later!"
+        console.error(error);
+    }
+
+    pokemonImg.src = pokemonFinded.sprites.front_default;
     pokemonImg.classList.remove('d-none');
 
 })
 
 const shinyBtn = document.querySelector('#pokemon-shiny')
 
-shinyBtn.addEventListener('click', () => {
-    pokemonImg.src = pokemon.sprites.front_shiny;
-})
+shinyBtn.addEventListener('click', (event) => {
+    shinyBtn.classList.toggle('active');
+    if (event.target.classList.contains('active')) {
+        pokemonImg.src = pokemonFinded.sprites.front_shiny;
+    } else {
+        pokemonImg.src = pokemonFinded.sprites.front_default;
+    }
+});
